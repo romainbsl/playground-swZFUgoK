976693c557a81559e90a7055c90622ff02c5ccc9
Romain Boisselle <romain.boisselle@gmail.com>
2017-10-03 19:22:04 +0200

    add channel messages in exprTest

--- kotlin-project/test/exprTest.kt
+++ kotlin-project/test/exprTest.kt
@@ -6,20 +6,40 @@ class ExpreTest {
   @Test
   fun operator() {
     // plus
-    Assert.assertEquals("Running Expr(199.0) + Expr(49.0)", Expr(248.0), Expr(199.0) + Expr(49.0))
-    Assert.assertEquals("Running Expr(9.5) + Expr(29.0)", Expr(38.5), Expr(9.5) + Expr(29.0))
+    msg("Plus Operand", "Running Expr(199.0) + Expr(49.0)")
+    msg("Plus Operand", "${Expr(199.0) + Expr(49.0)}")
+    Assert.assertEquals(Expr(248.0), Expr(199.0) + Expr(49.0))
+    msg("Plus Operand", "Running Expr(9.5) + Expr(29.0)")
+    msg("Plus Operand", "${Expr(9.5) + Expr(29.0)}")
+    Assert.assertEquals(Expr(38.5), Expr(9.5) + Expr(29.0))
     // minus
-    Assert.assertEquals("Running Expr(199.0) - Expr(49.0)...", Expr(150.0), Expr(199.0) - Expr(49.0))
-    Assert.assertEquals("Running Expr(9.5) - Expr(29.0)...", Expr(-19.5), Expr(9.5) - Expr(29.0))
+    msg("Minus Operand", "Running Expr(199.0) - Expr(49.0)...")
+    msg("Minus Operand", "${Expr(199.0) - Expr(49.0)}")
+    Assert.assertEquals(Expr(150.0), Expr(199.0) - Expr(49.0))
+    msg("Minus Operand", "Running Expr(9.5) - Expr(29.0)...")
+    msg("Minus Operand", "${Expr(9.5) - Expr(29.0)}")
+    Assert.assertEquals(Expr(-19.5), Expr(9.5) - Expr(29.0))
     // times
-    Assert.assertEquals("Running Expr(10.0) * Expr(49.0)...", Expr(490.0), Expr(10.0) * Expr(49.0))
-    Assert.assertEquals("Running Expr(9.5) * Expr(29.0)...", Expr(275.5), Expr(9.5) * Expr(29.0))
+    msg("Times Operand", "Running Expr(10.0) * Expr(49.0)...")
+    msg("Times Operand", "${Expr(10.0) * Expr(49.0)}")
+    Assert.assertEquals(Expr(490.0), Expr(10.0) * Expr(49.0))
+    msg("Times Operand", "Running Expr(9.5) * Expr(29.0)...")
+    msg("Times Operand", "${Expr(9.5) * Expr(29.0)}")
+    Assert.assertEquals(Expr(275.5), Expr(9.5) * Expr(29.0))
     // div
-    Assert.assertEquals("Running Expr(90.0) / Expr(9.0)...", Expr(10.0), Expr(90.0) / Expr(9.0))
-    Assert.assertEquals("Running Expr(9.0) / Expr(3.0)...", Expr(3.0), Expr(9.0) / Expr(3.0))
+    msg("Div Operand", "Running Expr(90.0) / Expr(9.0)...")
+    msg("Div Operand", "${Expr(90.0) / Expr(9.0)}")
+    Assert.assertEquals(Expr(10.0), Expr(90.0) / Expr(9.0))
+    msg("Div Operand", "Running Expr(9.0) / Expr(3.0)...")
+    msg("Div Operand", "${Expr(9.0) / Expr(3.0)}")
+    Assert.assertEquals(Expr(3.0), Expr(9.0) / Expr(3.0))
     // rem
-    Assert.assertEquals("Running Expr(12.0) % Expr(5.0)...", Expr(2.0), Expr(12.0) % Expr(5.0))
-    Assert.assertEquals("Running Expr(9.5) % Expr(3.0)...", Expr(0.5), Expr(9.5) % Expr(3.0))
+    msg("Rem (mod) Operand", "Running Expr(12.0) % Expr(5.0)...")
+    msg("Rem (mod) Operand", "${Expr(12.0) % Expr(5.0)}")
+    Assert.assertEquals(Expr(2.0), Expr(12.0) % Expr(5.0))
+    msg("Rem (mod) Operand", "Running Expr(9.5) % Expr(3.0)...")
+    msg("Rem (mod) Operand", "${Expr(9.5) % Expr(3.0)}")
+    Assert.assertEquals(Expr(0.5), Expr(9.5) % Expr(3.0))
   }
 
   fun msg(channel: String, msg: String?) {
