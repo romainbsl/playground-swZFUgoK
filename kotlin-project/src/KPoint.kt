ee152442e5549fa84c100bb547974d9e10714730
Romain Boisselle <romain.boisselle@gmail.com>
2017-10-04 22:19:05 +0200

    refactoring

--- kotlin-project/src/KPoint.kt
+++ kotlin-project/src/KPoint.kt
@@ -2,16 +2,12 @@ data class KPoint(var x: Double, var y: Double) {
   /*
     implement operators
    */
-//  operator fun plus(p: KPoint): KPoint {return KPoint(x + p.x, y + p.y) }
-//  operator fun minus(p: KPoint): KPoint { return KPoint(x - p.x, y - p.y) }
-//  operator fun dec() : KPoint { return KPoint(--x, --y) }
-//  operator fun inc() : KPoint { return KPoint(++x, ++y) }
-  operator fun plus(p: KPoint): KPoint {return KPoint(Double.MIN_VALUE, Double.MAX_VALUE) }
-  operator fun minus(p: KPoint): KPoint { return KPoint(Double.MIN_VALUE, Double.MAX_VALUE) }
-  operator fun dec() : KPoint { return KPoint(Double.MIN_VALUE, Double.MAX_VALUE) }
-  operator fun inc() : KPoint { return KPoint(Double.MIN_VALUE, Double.MAX_VALUE) }
+  operator fun plus(p: KPoint): KPoint = KPoint(Double.MIN_VALUE, Double.MAX_VALUE)
+  operator fun minus(p: KPoint): KPoint = KPoint(Double.MIN_VALUE, Double.MAX_VALUE)
+  operator fun dec(): KPoint = KPoint(Double.MIN_VALUE, Double.MAX_VALUE)
+  operator fun inc(): KPoint = KPoint(Double.MIN_VALUE, Double.MAX_VALUE)
 
-  override fun toString(): String {
-    return "KPoint($x, $y)"
-  }
+//{autofold
+  override fun toString() = "KPoint($x, $y)"
+//}
 }
