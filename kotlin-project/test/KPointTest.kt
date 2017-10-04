a8e3a5178e35506c337b1a65435db168db84f751
Romain Boisselle <romain.boisselle@gmail.com>
2017-10-04 21:51:42 +0200

    change output

--- kotlin-project/test/KPointTest.kt
+++ kotlin-project/test/KPointTest.kt
@@ -19,18 +19,18 @@ open class KPointTest {
     msg(plusChannel, running("KPoint(2.0, 3.0) + KPoint(3.0, 2.0)"))
     msg(plusChannel, plusPoint1.toString())
     val resultPlus1 = plusPoint1 == KPoint(5.0, 5.0)
-    if(!resultPlus1) {
+    if (!resultPlus1) {
       msg(plusChannel, badImpl("plus()"))
-      msg(plusChannel, error("KPoint(5.0, 5.0)","$plusPoint1"))
+      msg(plusChannel, error("KPoint(5.0, 5.0)", "$plusPoint1"))
     }
 
     val plusPoint2 = KPoint(20.0, -1.0) + KPoint(5.0, 15.0)
     msg(plusChannel, running("KPoint(20.0, -1.0) + KPoint(5.0, 15.0)"))
     msg(plusChannel, plusPoint2.toString())
     val resultPlus2 = plusPoint2 == KPoint(25.0, 14.0)
-    if(!resultPlus2) {
+    if (!resultPlus2) {
       msg(plusChannel, badImpl("plus()"))
-      msg(plusChannel, error("KPoint(25.0, 14.0)","$plusPoint2"))
+      msg(plusChannel, error("KPoint(25.0, 14.0)", "$plusPoint2"))
     }
 
     // Minus
@@ -38,57 +38,53 @@ open class KPointTest {
     msg(miusChannel, running("KPoint(5.0, 5.0) - KPoint(2.0, 2.0)"))
     msg(miusChannel, minusPoint1.toString())
     val resultMinus1 = minusPoint1 == KPoint(3.0, 3.0)
-    if(!resultMinus1) {
+    if (!resultMinus1) {
       msg(miusChannel, badImpl("minus()"))
-      msg(miusChannel, error("KPoint(3.0, 3.0)","$minusPoint1"))
+      msg(miusChannel, error("KPoint(3.0, 3.0)", "$minusPoint1"))
     }
 
     val minusPoint2 = KPoint(0.0, 0.0) - KPoint(10.0, 15.0)
     msg(miusChannel, running("KPoint(0.0, 0.0) - KPoint(10.0, 15.0)"))
     msg(miusChannel, minusPoint2.toString())
     val resultMinus2 = minusPoint2 == KPoint(-10.0, -15.0)
-    if(!resultMinus2) {
+    if (!resultMinus2) {
       msg(miusChannel, badImpl("minus()"))
-      msg(miusChannel, error("KPoint(-10.0, -15.0)","$minusPoint2"))
+      msg(miusChannel, error("KPoint(-10.0, -15.0)", "$minusPoint2"))
     }
 
     // Dec
     var decPoint1 = KPoint(5.0, 5.0)
     msg(decChannel, running("--KPoint(5.0, 5.0)"))
-    msg(decChannel, "${--decPoint1}")
     val resultDec1 = decPoint1 == KPoint(4.0, 4.0)
-    if(!resultDec1) {
+    if (!resultDec1) {
       msg(decChannel, badImpl("dec()"))
-      msg(decChannel, error("KPoint(4.0, 4.0)","$decPoint1"))
-    }
+      msg(decChannel, error("KPoint(4.0, 4.0)", "$decPoint1"))
+    } else msg(decChannel, "${--decPoint1}")
 
     var decPoint2 = KPoint(-10.0, -5.0)
     msg(decChannel, running("--KPoint(-10.0, -5.0)"))
-    msg(decChannel, "${--decPoint2}")
     val resultDec2 = decPoint2 == KPoint(-11.0, -6.0)
-    if(!resultDec2) {
+    if (!resultDec2) {
       msg(decChannel, badImpl("dec()"))
-      msg(decChannel, error("KPoint(-11.0, -6.0)","$decPoint2"))
-    }
+      msg(decChannel, error("KPoint(-11.0, -6.0)", "$decPoint2"))
+    } else msg(decChannel, "${--decPoint2}")
 
     // Inc
     var incPoint1 = KPoint(5.0, 5.0)
     msg(incChannel, running("++KPoint(5.0, 5.0)"))
-    msg(incChannel, "${++incPoint1}")
     val resultInc1 = incPoint1 == KPoint(6.0, 6.0)
-    if(!resultInc1) {
+    if (!resultInc1) {
       msg(incChannel, badImpl("inc()"))
-      msg(incChannel, error("KPoint(6.0, 6.0)","$incPoint1"))
-    }
+      msg(incChannel, error("KPoint(6.0, 6.0)", "$incPoint1"))
+    } else msg(incChannel, "${++incPoint1}")
 
     var incPoint2 = KPoint(-10.0, -5.0)
     msg(incChannel, running("++KPoint(-10.0, -5.0)"))
-    msg(incChannel, "${++incPoint2}")
     val resultInc2 = incPoint2 == KPoint(-9.0, -4.0)
-    if(!resultInc2) {
+    if (!resultInc2) {
       msg(incChannel, badImpl("inc()"))
-      msg(incChannel, error("KPoint(-9.0, -4.0)","$incPoint2"))
-    }
+      msg(incChannel, error("KPoint(-9.0, -4.0)", "$incPoint2"))
+    } else msg(incChannel, "${++incPoint2}")
 
     success(resultPlus1 && resultPlus2
         && resultMinus1 && resultMinus2
