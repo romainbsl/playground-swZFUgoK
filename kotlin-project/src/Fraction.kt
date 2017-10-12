92471c007aa13b84b27a3d77604bc7a3216c6851
Romain Boisselle <romainbsl@users.noreply.github.com>
2017-10-12 14:56:11 +0200

    Update Fraction.kt

--- kotlin-project/src/Fraction.kt
+++ kotlin-project/src/Fraction.kt
@@ -1,1 +1,11 @@
-data class Fraction(val numerator: Int, val denominator: Int) { // TODO implement operator(s) }
+data class Fraction(val numerator: Int, val denominator: Int) { 
+    operator fun plus(add: Fraction) : Fraction {
+    // TODO implement operator
+    return Fraction(1,1)
+  }
+  
+  operator fun times(number: Int) : Fraction {
+    // TODO implement operator
+    return Fraction(1,1)
+  }
+}
