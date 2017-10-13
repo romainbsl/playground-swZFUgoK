33a305ce5ffa2bc36dcfadc67a21bc984b4585fd
Romain Boisselle <romainbsl@users.noreply.github.com>
2017-10-13 09:42:32 +0200

    Update ComparableFraction.kt

--- kotlin-project/src/ComparableFraction.kt
+++ kotlin-project/src/ComparableFraction.kt
@@ -1,5 +1,6 @@
 data class ComparableFraction(val numerator: Int, val denominator: Int) : Comparable<ComparableFraction> {
   private val decimal by lazy { numerator.toDouble() / denominator }
+  
   override fun compareTo(other: ComparableFraction): Int {
     TODO("not implemented")
   }
