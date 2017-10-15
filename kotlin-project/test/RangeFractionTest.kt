586207dc9f571d583bf49a50649d2c19255914b6
Romain Boisselle <romain.boisselle@gmail.com>
2017-10-15 10:47:39 +0200

    fix range fraction test

--- kotlin-project/test/RangeFractionTest.kt
+++ kotlin-project/test/RangeFractionTest.kt
@@ -9,7 +9,7 @@ class RangeFractionTest {
   fun iterator() {
     val fractIterator = fraction1..fraction2
 
-    msgIterator(running("$fraction1..$fraction2.contains(RangeFraction(1, 2))"))
+    msgIterator(running("($fraction1..$fraction2).contains(RangeFraction(1, 2))"))
     val result1 = fractIterator.contains(RangeFraction(1, 2))
     if (!result1) {
       msgIterator(badImpl("iterator()"))
@@ -18,7 +18,7 @@ class RangeFractionTest {
 
     msgIterator("")
 
-    msgIterator(running("$fraction1..$fraction2.contains(RangeFraction(2, 3))"))
+    msgIterator(running("($fraction1..$fraction2).contains(RangeFraction(2, 3))"))
     val result2 = fractIterator.contains(RangeFraction(2, 3))
     if (!result2) {
       msgIterator(badImpl("iterator()"))
@@ -27,7 +27,7 @@ class RangeFractionTest {
 
     msgIterator("")
 
-    msgIterator(running("$fraction1..$fraction2.contains(RangeFraction(3, 4))"))
+    msgIterator(running("($fraction1..$fraction2).contains(RangeFraction(3, 4))"))
     val result3 = fractIterator.contains(RangeFraction(3, 4))
     if (!result3) {
       msgIterator(badImpl("iterator()"))
@@ -36,7 +36,7 @@ class RangeFractionTest {
 
     msgIterator("")
 
-    msgIterator(running("$fraction1..$fraction2.contains(RangeFraction(6, 8))"))
+    msgIterator(running("($fraction1..$fraction2).contains(RangeFraction(6, 8))"))
     val result4 = fractIterator.contains(RangeFraction(6, 8))
     if (!result4) {
       msgIterator(badImpl("iterator()"))
@@ -45,7 +45,7 @@ class RangeFractionTest {
 
     msgIterator("")
 
-    msgIterator(running("$fraction1..$fraction2.contains(RangeFraction(10, 12))"))
+    msgIterator(running("($fraction1..$fraction2).contains(RangeFraction(10, 12))"))
     val result5 = fractIterator.contains(RangeFraction(10, 12))
     if (result5) {
       msgIterator(badImpl("iterator()"))
